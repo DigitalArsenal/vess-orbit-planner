3c7d854a3df25470e06460a981cf09302fbff020
TJ Koury <tj@digitalarsenal.io>
2024-07-18 11:34:08 -0400

    globe up!

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+  import { onMount } from "svelte";
+  import "./style.css";
+  import "orbpro/style/widgets.css";
+  import { Viewer } from "orbpro";
+
+  onMount(() => {
+    const viewer = new Viewer("orbpro");
+  });
+</script>
+
+<div id="orbpro"></div>
+
+<style>
+  #orbpro{
+    width:100vw;
+    height:100vh;
+  }
+</style>
