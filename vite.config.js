ff96397eb4c0e738574bf3f1dcf1aa248cba6e5d
TJ Koury <tj@digitalarsenal.io>
2024-07-18 21:23:38 -0400

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,25 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { resolve } from 'path';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    createHtmlPlugin({
+      minify: true,
+    }),
+    viteSingleFile(),
+  ],
   build: {
     outDir: 'docs',
     rollupOptions: {
+      input: resolve(__dirname, 'index.html'),
       output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]'
-      }
-    }
-  }
+        inlineDynamicImports: true,
+        manualChunks: undefined,
+      },
+    },
+  },
 });
