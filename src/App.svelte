1908d347bf9d3ef380d9e8694ae29274410d1b24
TJ Koury <tj@digitalarsenal.io>
2024-07-20 03:45:50 -0400

    updates

--- src/App.svelte
+++ src/App.svelte
@@ -476,8 +476,13 @@
 {/if}
 
 <style>
+  * {
+    -webkit-user-select: none; /* Safari */
+    -moz-user-select: none; /* Firefox */
+    -ms-user-select: none; /* Internet Explorer/Edge */
+    user-select: none; /* Non-prefixed version, currently supported by Chrome, Opera and Edge */
+  }
   #orbpro {
-    user-select: none;
     width: 100%;
     height: 100%;
     padding-bottom: env(safe-area-inset-bottom);
